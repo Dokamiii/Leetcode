70c03f5014b3a9b3ed478e092328af24b62b503e
Dokamiii <eduardofranciscoln110@gmail.com>
2025-03-09 18:04:06 -0300

    Criação do map dos numeros romanos

---
+++ romantointeger/solution.java
@@ -0,0 +1,27 @@
+package romantointeger;
+
+class solution {
+    public static void main(String[] args) {
+         /*HashMap<String, Integer> romantoint = new HashMap<String, Integer>();
+            romantoint.put("I", 1);
+            romantoint.put("V", 5);
+            romantoint.put("X", 10);
+            romantoint.put("L", 50);
+            romantoint.put("C", 100);
+            romantoint.put("D", 500);
+            romantoint.put("M", 1000);
+            */
+
+            String s = "III";
+            String caractere = "I";
+            int tamanho = s.length();
+            System.out.println(tamanho);
+            
+            
+            if(s.indexOf(caractere) >= 0){
+                System.out.println(caractere);
+            } else {
+                System.out.println(caractere);
+            }
+    }
+}
