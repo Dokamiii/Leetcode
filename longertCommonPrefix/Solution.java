f0f32c322c71270ede8c62bcc916c65eccd41140
Dokamiii <eduardofranciscoln110@gmail.com>
2025-04-18 17:50:51 -0300

    codigo de solucao

---
+++ longertCommonPrefix/Solution.java
@@ -0,0 +1,19 @@
+package longertCommonPrefix;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String prefixo;
+        String[] strs = {"flower", "flow", "flight"};
+        for (int i = 0; i < strs.length; i++) {
+            for(int j = i + 1; j < strs.length; j++){
+                System.out.println(strs[i] + "," + strs[j]);
+                char[] palavra1 = strs[i].toCharArray();
+                char[] palavra2 = strs[j].toCharArray();
+                if (pal) {
+                    
+                }
+            }
+        }
+    }
+}
